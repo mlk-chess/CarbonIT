8b17617de70d310c255a98c55e148986176b2852
jehanebnj <jehanebnj@gmail.com>
2023-06-06 10:51:52 +0200

    create list of goals

---
+++ pages/admin/goal/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Liste des goals</h1>
+    <ul>
+      <li v-for="(goal, index) in goals" :key="index">
+        {{ goal }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+
+  const goals = ref(null)
+
+  onMounted( async () => {
+
+      const { data, error } = await supabase.from('goals').select('*');
+      if (error) {
+      console.error(error);
+      } else {
+      goals.value = data;
+      }
+  })
+
+</script>
