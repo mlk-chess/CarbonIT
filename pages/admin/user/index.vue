76a98f7864900d2e8fd58e334e04513153c10e69
Théo Sigaud <sigaudtheo@gmail.com>
2023-06-06 20:24:47 +0200

    Update user

--- pages/admin/user/index.vue
+++ pages/admin/user/index.vue
@@ -66,14 +66,14 @@ onMounted(async () => {
             </th>
             <th scope="row"
                 class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap dark:text-white flex">
-              <div class="hover:cursor-pointer hover:text-blue-400">
+              <NuxtLink :href="`/admin/user/${user.id}`" class="hover:cursor-pointer hover:text-blue-400">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                      stroke="currentColor" class="w-6 h-6">
                   <path stroke-linecap="round" stroke-linejoin="round"
                         d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z"/>
                   <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"/>
                 </svg>
-              </div>
+              </NuxtLink>
               <NuxtLink :href="`/admin/skill/${user.id}`" class="ml-5 hover:cursor-pointer hover:text-yellow-400">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                      stroke="currentColor" class="w-6 h-6">
