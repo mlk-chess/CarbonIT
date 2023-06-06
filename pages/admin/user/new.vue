98cf5d63a97f2269cc67c8c4f4e625ee3063c9e9
Théo Sigaud <sigaudtheo@gmail.com>
2023-06-06 11:50:45 +0200

    Create new user

---
+++ pages/admin/user/new.vue
@@ -0,0 +1,104 @@
+<script setup>
+definePageMeta({
+  middleware: ["auth"],
+});
+
+const firstname = ref("");
+const name = ref("");
+const status = ref("0");
+const phone = ref("");
+const email = ref("");
+const address = ref("");
+const city = ref("");
+const zip = ref("");
+const rib = ref("");
+
+async function saveUser() {
+  await $fetch('/api/user/new', {
+    method: 'post',
+    body: {
+      name: name.value,
+      firstname: firstname.value,
+      status: status.value,
+      city: city.value,
+      phone: phone.value,
+      address: address.value,
+      email: email.value,
+      rib: rib.value,
+      zip: zip.value,
+    }
+  });
+}
+</script>
+
+<template>
+  <section>
+    <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+      <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Nouvel utilisateur</h2>
+      <form v-on:submit.prevent="saveUser">
+        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6">
+          <div class="w-full">
+            <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Prénom</label>
+            <input v-model="firstname" type="text" name="firstname" id="firstname"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Prénom de l'utilisateur" required="">
+          </div>
+          <div class="w-full">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nom</label>
+            <input v-model="name" type="text" name="name" id="name"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Nom de l'utilisateur" required="">
+          </div>
+          <div>
+            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Statut</label>
+            <select v-model="status" id="status"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+              <option selected value="0">Collaborateur</option>
+              <option value="1">Administrateur</option>
+            </select>
+          </div>
+          <div>
+            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Téléphone</label>
+            <input v-model="phone" type="tel" name="phone" id="phone"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="12" required="">
+          </div>
+          <div class="sm:col-span-2">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+            <input v-model="email" type="email" name="name" id="name"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Email du collaborateur" required="">
+          </div>
+          <div class="sm:col-span-2">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse</label>
+            <input v-model="address" type="text" name="name" id="name"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Adresse du collaborateur" required="">
+          </div>
+          <div class="w-full">
+            <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ville</label>
+            <input v-model="city" type="text" name="firstname" id="firstname"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Ville de l'utilisateur" required="">
+          </div>
+          <div class="w-full">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Postal</label>
+            <input v-model="zip" type="text" name="name" id="name"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Code postal de l'utilisateur" required="">
+          </div>
+          <div class="sm:col-span-2">
+            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">RIB</label>
+            <input v-model="rib" type="text" name="name" id="name"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="RIB de l'utilisateur" required="">
+          </div>
+        </div>
+        <button type="submit"
+                class="mt-7 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
+          Ajouter
+        </button>
+      </form>
+    </div>
+  </section>
+</template>
---
+++ server/api/user/new.ts
@@ -0,0 +1,51 @@
+import { serverSupabaseClient } from '#supabase/server';
+
+export default defineEventHandler(async (event) => {
+
+    const supabase = serverSupabaseClient(event);
+    const body = await readBody(event);
+
+    function generatePassword() {
+        const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+        let password = '';
+
+        for (let i = 0; i < 16; i++) {
+            const randomIndex = Math.floor(Math.random() * charset.length);
+            password += charset[randomIndex];
+        }
+
+        return password;
+    }
+
+    const generatedPassword = generatePassword();
+
+    const { data: dataUser, error: errorUser } = await supabase.auth.signUp({
+        email: body.email,
+        password: generatedPassword,
+    })
+
+    if(errorUser) {
+        return 'Error';
+    }
+
+    const {data: newDataUser, newErrorUser} = await supabase
+        .from('user')
+        .insert([{
+            lastname: body.name,
+            firstname: body.firstname,
+            status: body.status,
+            email: body.email,
+            city: body.city,
+            zipcode: body.zip,
+            address: body.address,
+            phone: body.phone,
+            rib: body.rib,
+            auth_id: dataUser.user.id
+        }]);
+
+    if(newErrorUser) {
+        return 'Error';
+    }
+
+    return 'Success';
+});
