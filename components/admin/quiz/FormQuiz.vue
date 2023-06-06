a7eca78b7c75d4d56efde36bae95ead04b28af4c
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-06-06 17:03:07 +0200

    Clean code again for quizzes question

--- components/admin/quiz/FormQuiz.vue
+++ components/admin/quiz/FormQuiz.vue
@@ -5,36 +5,9 @@
             <Difficulty :difficulty="difficulty" :setDifficulty="setDifficulty" />
         </div>
 
-        <div v-for="(question, questionIndex) in questions" :key="questionIndex" class="mb-4">
-            <h3 class="text-xl font-bold mb-2">Question {{ questionIndex + 1 }}</h3>
-            <div class="mb-2">
-                <label for="question" class="block mb-1">Question:</label>
-                <input type="text" v-model="question.questionText" required
-                    class="block w-full p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-            </div>
-            <div v-for="(answer, answerIndex) in question.answers" :key="answerIndex" class="flex items-center mb-2">
-                <input type="text" v-model="answer.answerText" required class="input mr-2">
-                <label class="relative inline-flex items-center cursor-pointer">
-                    <input type="checkbox" class="sr-only peer" v-model="answer.isCorrect">
-                    <div
-                        class="w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600">
-                    </div>
-                    <span class="ml-3 text-sm font-medium text-gray-900 dark:text-gray-300">Réponse correcte</span>
-                </label>
+        <Questions :questions="questions" :addQuestion="addQuestion" :removeQuestion="removeQuestion" :addAnswer="addAnswer"
+            :removeAnswer="removeAnswer" />
 
-            </div>
-            <div class="flex">
-                <button type="button" @click="addAnswer(questionIndex)" class="btn-primary mr-2">Ajouter une
-                    réponse</button>
-                <button type="button" @click="removeAnswer(questionIndex, answerIndex)" class="btn-secondary">Supprimer
-                    la réponse</button>
-            </div>
-            <div class="flex">
-                <button type="button" @click="addQuestion" class="btn-primary mr-2">Ajouter une question</button>
-                <button type="button" @click="removeQuestion(questionIndex)" class="btn-secondary">Supprimer la
-                    question</button>
-            </div>
-        </div>
         <button type="submit" class="btn-secondary">Créer</button>
     </form>
 </template>
@@ -42,10 +15,12 @@
 <script>
 import Difficulty from "./Difficulty.vue";
 import QuizName from "./QuizName.vue";
+import Questions from "./Questions.vue";
 export default {
     components: {
         Difficulty,
-        QuizName
+        QuizName,
+        Questions
     },
     data() {
         return {
@@ -75,7 +50,13 @@ export default {
             });
         },
         removeAnswer(questionIndex, answerIndex) {
-            this.questions[questionIndex].answers.splice(answerIndex, 1);
+            const question = this.questions[questionIndex];
+
+            if (question.answers.length > 1) {
+                question.answers.splice(answerIndex, 1);
+            } else {
+                console.log("Impossible de supprimer la réponse. Il doit y avoir au moins une réponse pour chaque question.");
+            }
         },
         addQuestion() {
             this.questions.push({
