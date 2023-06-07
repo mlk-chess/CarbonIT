d4a0f2de4e556659f8b58eef0cff47daf34d6e18
waseem1104 <waseem11@hotmail.fr>
2023-06-07 14:15:24 +0200

    Init calendar event

---
+++ pages/admin/index.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="w-4/12">
+        <FullCalendar :options="calendarOptions" />
+    </div>
+</template>
+
+
+<style scoped>
+
+</style>
+<script setup>
+import FullCalendar from '@fullcalendar/vue3'
+import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from '@fullcalendar/interaction'
+import frLocale from '@fullcalendar/core/locales/fr'
+
+const calendarOptions = ref({
+    plugins: [dayGridPlugin, interactionPlugin],
+    initialView: 'dayGridMonth',
+    locale: frLocale,
+    events: [
+        { title: 'Événement 1', start: '2023-06-01' },
+        { title: 'Événement 1', start: '2023-06-01' },
+        { title: 'Événement 1', start: '2023-06-01' },
+        { title: 'Événement 2', start: '2023-06-05' },
+        { title: 'Événement 3', start: '2023-06-10' }
+    ],
+})
+
+
+</script>
