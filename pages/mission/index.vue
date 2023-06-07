8d07b01577df732c92d4a73b6b19a8cc48b6d459
jehanebnj <jehanebnj@gmail.com>
2023-06-07 16:42:41 +0200

    Create list of missions

---
+++ pages/mission/index.vue
@@ -0,0 +1,104 @@
+<template>
+
+<div class="container mx-auto py-8 bf">
+  <ul class="flex mb-5">
+    <li class="mr-6">
+        <a class="text-bold text-2xl hover:text-custom-green" href="#">Mes missions</a>
+    </li>
+    <li class="mr-6">
+        <a class="text-bold text-2xl hover:text-custom-green" href="#">Découvrir les missions</a>
+    </li>
+  </ul> <hr/>
+    <form class="flex items-center mt-10">   
+        <label for="voice-search" class="sr-only">Search</label>
+        <div class="relative w-full">
+            <div class="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <svg aria-hidden="true" class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z" clip-rule="evenodd"></path></svg>
+            </div>
+            <input type="text" id="voice-search" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full pl-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Rechercher..." required>
+            <button type="button" class="absolute inset-y-0 right-0 flex items-center pr-3">
+                <svg aria-hidden="true" class="w-4 h-4 text-gray-500 dark:text-gray-400 hover:text-gray-900 dark:hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                    <path fill-rule="evenodd" d="M7 4a3 3 0 016 0v4a3 3 0 11-6 0V4zm4 10.93A7.001 7.001 0 0017 8a1 1 0 10-2 0A5 5 0 015 8a1 1 0 00-2 0 7.001 7.001 0 006 6.93V17H6a1 1 0 100 2h8a1 1 0 100-2h-3v-2.07z" clip-rule="evenodd"/>
+                </svg>
+            </button>
+        </div>
+        <div class="flex items-center justify-center p-4">
+  <button id="dropdownDefault" data-dropdown-toggle="dropdown"
+    class="text-white bg-custom-green hover:text-black focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-4 py-2.5 text-center inline-flex items-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+    type="button">
+    Filtrer
+    <svg class="w-4 h-4 ml-2" aria-hidden="true" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg">
+      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"/>
+    </svg>
+  </button>
+
+  <!-- Dropdown menu -->
+  <div id="dropdown" class="z-10 hidden w-56 p-3 bg-white rounded-lg shadow dark:bg-gray-700">
+    <h6 class="mb-3 text-sm font-medium text-gray-900 dark:text-white">
+      Compétences
+    </h6>
+    <ul class="space-y-2 text-sm" aria-labelledby="dropdownDefault">
+      <li class="flex items-center">
+        <input id="apple" type="checkbox" value=""
+          class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500" />
+
+        <label for="apple" class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+          <!-- {{skill.title}}  -->
+        </label>
+      </li>
+    </ul>
+  </div>
+  
+</div>
+    </form>
+
+    <ul class="flex w-full mt-10 justify-between items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row hover:bg-gray-100 mb-5" v-for="(mission, index) in missions" :key="index">
+      <li class="flex flex-col justify-between p-4 md:p-8" >
+        <h5 class="mb-5 text-xl font-bold tracking-tight text-gray-900">{{ mission.title }} | {{mission.company}}</h5>
+        <p class="mb-3 font-normal text-gray-700">{{ mission.date_start }} | {{mission.localisation}}</p>
+        <p class="mb-3 font-normal text-gray-700">{{ mission.description }}</p>
+      </li>
+      <div class=" items-center mt-5 pb-5 sm:pr-5" v-if="mission.like == 0">
+        <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
+        </svg>
+      </div>
+      <div class=" items-center mt-5 pb-5 sm:pr-5" v-if="mission.like == 1">
+        <svg fill="#E53F49" color="#E53F49" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
+        </svg>
+      </div>
+    </ul> 
+  </div>
+</template>
+
+<script setup>
+  import {initFlowbite} from "flowbite";
+  const missions = ref(null)
+  const skills = ref(null)
+  const supabase = useSupabaseClient();
+
+  onMounted( async () => {
+
+     initFlowbite();
+      const { data, error } = await supabase.from('mission').select('*');
+      if (error) {
+      console.error(error);
+      } else {
+      missions.value = data;
+      }
+  })
+
+//   onMounted( async () => {
+
+//       const { data, error } = await supabase.from('skill').select('*');
+//       if (error) {
+//       console.error(error);
+//       } else {
+//         console.log("data")
+//       skills.value = data;
+//       }
+//   })
+
+</script>
