13764e3d110b62a36b9a57a2ca7711def822e98e
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-06-07 11:35:52 +0200

    Quiz - Navigation done

--- components/admin/quiz/index/Browse.vue
+++ components/admin/quiz/index/Browse.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="grid grid-cols-2 md:grid-cols-3 gap-4">
         <div v-for="quiz in quizzes" :key="quiz.id">
-            <a href="#"
+            <NuxtLink href="/admin/quiz/create-questions"
                 class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
                 <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ quiz.quiz_name }}</h5>
                 <p class="font-normal text-gray-700 dark:text-gray-400">{{ quiz.difficulty }}</p>
                 <p class="font-normal text-gray-700 dark:text-gray-400">{{ quiz.description }}</p>
                 <p class="font-normal text-gray-700 dark:text-gray-400">{{ quiz.theme }}</p>
-            </a>
+            </NuxtLink>
         </div>
     </div>
 </template>
--- components/admin/quiz/index/Header.vue
+++ components/admin/quiz/index/Header.vue
@@ -16,7 +16,7 @@
                     le point de découvrir un moyen efficace et amusant d'évaluer le potentiel de vos collaborateurs, tout en
                     favorisant un environnement de travail stimulant et enrichissant.</p>
                 <div class="flex flex-col space-y-4 sm:flex-row sm:space-y-0 sm:space-x-4">
-                    <a href="#"
+                    <NuxtLink href="/admin/quiz/new"
                         class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
                         Créer un quiz
                         <svg aria-hidden="true" class="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
@@ -25,11 +25,11 @@
                                 d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
                                 clip-rule="evenodd"></path>
                         </svg>
-                    </a>
-                    <a href="#"
-                        class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
+                    </NuxtLink>
+                    <NuxtLink href="/admin/quiz/create-questions"
+                            class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800">
                         Parcourir
-                    </a>
+                    </NuxtLink>
                 </div>
             </div>
 
