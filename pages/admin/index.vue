8570e1369486caf1716f91ccb1cdd8d299bea304
jehanebnj <jehanebnj@gmail.com>
2023-06-08 12:13:42 +0200

    fix for PR

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex gap-2">
 
-    <div class="bg-custom-white w-1/2 shadow p-5 rounded mb-5 mt-5">
+    <div class="w-1/2 shadow p-5 rounded mb-5 mt-5">
       <div class="">
         <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Jeu du jour</h5>
 
@@ -20,7 +20,7 @@
       </div>
     </div>
 
-    <div class="bg-custom-white w-1/2 shadow p-5 rounded mb-5">
+    <div class="w-1/2 shadow p-5 rounded mb-5">
 
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">CarbonAPPs</h5>
 
@@ -29,7 +29,7 @@
   </div>
 
   <div class="flex gap-2">
-    <div class="bg-custom-white w-4/12 shadow p-5 rounded">
+    <div class="w-4/12 shadow p-5 rounded">
       <FullCalendar :options="calendarOptions">
         <template v-slot:eventContent='arg'>
           <p class="overflow-hidden hover:cursor-pointer" @click="showModal = true; dateModal = arg.event">{{ arg.event.title }}</p>
@@ -37,11 +37,11 @@
       </FullCalendar>
     </div>
 
-    <div class="bg-custom-white w-4/12 shadow p-5 rounded">
+    <div class="w-4/12 shadow p-5 rounded">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Actualités</h5>
     </div>
 
-    <div class="bg-custom-white w-4/12 shadow p-5 rounded">
+    <div class="w-4/12 shadow p-5 rounded">
       <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">La blagounette</h5>
     </div>
 
@@ -83,17 +83,6 @@ import frLocale from '@fullcalendar/core/locales/fr'
 definePageMeta({
   middleware: ["auth-admin"],
   layout: "admin",
-});
-
-useHead({
-  bodyAttrs: {
-    class: 'bg-[#F1F8FF]'
-  }
-});
-
-definePageMeta({
-  middleware: ["auth"],
-  layout: "admin"
 });
 
 const supabase = useSupabaseClient();
