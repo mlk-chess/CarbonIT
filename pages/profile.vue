102714a4bca46c2ff7ea7378275c57408fe88ef6
waseem1104 <waseem11@hotmail.fr>
2023-06-08 12:21:06 +0200

    Design gamification

--- pages/profile.vue
+++ pages/profile.vue
@@ -1,10 +1,10 @@
 <template>
     <section class="">
         <ol class="flex items-center w-full mb-4 sm:mb-5 bg-custom-white p-4 rounded shadow">
-            <li class="flex w-full items-center text-blue-600 dark:text-blue-500 after:content-[''] after:w-full after:h-1 after:border-b after:border-4 after:inline-block">
-                <div class="flex items-center justify-center w-10 h-10 bg-custom-black rounded-full lg:h-12 lg:w-12  shrink-0">
+           <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-custom-black after:border-4 after:inline-block dark:after:border-blue-800">
+                <span class="flex items-center justify-center w-10 h-10 bg-custom-black rounded-full lg:h-12 lg:w-12 dark:bg-blue-800 shrink-0">
                      <img class="mx-auto" src="@/assets/icons/creature-step1.svg">
-                </div>
+                </span>
             </li>
             <li class="flex w-full items-center after:content-[''] after:w-full after:h-1 after:border-b after:border-gray-100 after:border-4 after:inline-block dark:after:border-gray-700">
                 <div class="flex items-center justify-center w-10 h-10 bg-gray-100 rounded-full lg:h-12 lg:w-12 dark:bg-gray-700 shrink-0">
@@ -29,9 +29,24 @@
             </li>
         </ol>
 
-        <div class="container mx-auto">
-            <div class="mx-auto w-1/2 mb-5 mt-5">
+        <div class="container mx-auto flex">
+            <div class="mx-auto md:w-1/2 mb-5 mt-5">
                 <img class="mx-auto" src="@/assets/icons/creature-step1.svg">
+            </div>
+
+            <div class="mx-auto md:w-1/2 mb-4 mt-10">
+                <ul class="flex w-full justify-between items-center border bg-custom-black text-custom-white rounded-lg shadow md:flex-row mb-5">
+                    <li class="flex flex-col justify-between p-4 md:p-8" >
+                       <i class="">Atteindre le level 5</i>
+                    </li>
+                    <div class=" items-center mt-5 pb-5 sm:pr-5">
+                        <svg fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"/>
+                        </svg>
+                    </div>
+                </ul> 
+
+                
             </div>
         </div>
     </section>
@@ -40,12 +55,19 @@
 <script setup>
 
 import {initFlowbite} from 'flowbite';
-
+const supabase = useSupabaseClient();
 useHead({
   bodyAttrs: {
     class: 'bg-[#F1F8FF]'
   }
 });
+
+onMounted( () => {
+    initFlowbite();
+
+    
+
+})
 
 definePageMeta({
   middleware: ["auth"],
