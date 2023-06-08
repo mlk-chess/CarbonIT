f556c77352c5d055721f975433b4ccaf71a0370e
Mohamed KAJEIOU <m.kajeiou123@gmail.com>
2023-06-08 15:42:47 +0200

    Fix redirection in Browse quiz

--- components/admin/quiz/index/Browse.vue
+++ components/admin/quiz/index/Browse.vue
@@ -9,13 +9,13 @@
                 </p>
                 <p class="font-normal text-gray-700 dark:text-gray-400">{{ quiz.description }}</p>
                 <p class="font-normal text-gray-700 dark:text-gray-400">{{ quiz.theme }}</p>
-                <a href="/admin/quiz/create-questions"
+                <NuxtLink :to="`/admin/quiz/add-question/${quiz.id}`"
                     class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-green-400 to-blue-600 group-hover:from-green-400 group-hover:to-blue-600 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-green-200 dark:focus:ring-green-800">
                     <span
                         class="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                         Edit
                     </span>
-                </a>
+                </NuxtLink>
                 <button
                     class="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
                     <span
