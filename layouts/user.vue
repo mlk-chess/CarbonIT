5312412ed0003dfca11a4d2532b7bcbfa0e82e49
jehanebnj <jehanebnj@gmail.com>
2023-06-09 03:16:18 +0200

    create chat template

--- layouts/user.vue
+++ layouts/user.vue
@@ -56,7 +56,7 @@ async function logout() {
           </NuxtLink>
           <NuxtLink
               class="flex items-center justify-center w-12 h-12 mt-2 rounded hover:bg-gray-700 hover:text-gray-300"
-              href="#">
+              href="/chat">
             <svg color="#FDFDFD" width="24" height="24" fill="none" stroke="currentColor" stroke-width="1.5"
                  viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
               <path stroke-linecap="round" stroke-linejoin="round"
