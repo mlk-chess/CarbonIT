52f3b3c0bf526f5be13ca7867ff1654b63a36e04
Théo Sigaud <sigaudtheo@gmail.com>
2023-06-09 11:45:55 +0200

    Update card sales

--- pages/admin/sales/index.vue
+++ pages/admin/sales/index.vue
@@ -28,48 +28,49 @@ onMounted(async () => {
     <div class="container mx-auto">
       <div class="w-full px-4 py-8 text-center lg:py-5 lg:px-6">
         <dl class="grid gap-8 mx-auto text-gray-900 md:grid-cols-2 xl:grid-cols-4 dark:text-white">
-          <div class="flex flec-row justify-center items-center align-middle bg-custom-white py-10 rounded">
-            <div class="w-10 p-7 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-primary-900 -translate-x-1/2">
-              <svg class="w-9 -translate-y-1/2 -translate-x-1/2 text-[#3A36DB] dark:text-primary-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z"></path>
-              </svg>
-            </div>
-            <div class="flex flex-col justify-center items-center">
-              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">178 +  </dt>
-              <dd class="font-light text-gray-500 dark:text-gray-400">Services favoris</dd>
-            </div>
-          </div>
-          <div class="flex flec-row justify-center items-center align-middle bg-custom-white py-10 rounded">
+          <div class="flex flex-row justify-center items-center align-middle bg-custom-white py-10 rounded">
             <div class="w-10 p-7 h-10 rounded-full bg-green-100 lg:h-12 lg:w-12 dark:bg-primary-900 -translate-x-1/2">
               <svg class="w-9 -translate-y-1/2 -translate-x-1/2 text-[#03A89E] dark:text-primary-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M6.633 10.5c.806 0 1.533-.446 2.031-1.08a9.041 9.041 0 012.861-2.4c.723-.384 1.35-.956 1.653-1.715a4.498 4.498 0 00.322-1.672V3a.75.75 0 01.75-.75A2.25 2.25 0 0116.5 4.5c0 1.152-.26 2.243-.723 3.218-.266.558.107 1.282.725 1.282h3.126c1.026 0 1.945.694 2.054 1.715.045.422.068.85.068 1.285a11.95 11.95 0 01-2.649 7.521c-.388.482-.987.729-1.605.729H13.48c-.483 0-.964-.078-1.423-.23l-3.114-1.04a4.501 4.501 0 00-1.423-.23H5.904M14.25 9h2.25M5.904 18.75c.083.205.173.405.27.602.197.4-.078.898-.523.898h-.908c-.889 0-1.713-.518-1.972-1.368a12 12 0 01-.521-3.507c0-1.553.295-3.036.831-4.398C3.387 10.203 4.167 9.75 5 9.75h1.053c.472 0 .745.556.5.96a8.958 8.958 0 00-1.302 4.665c0 1.194.232 2.333.654 3.375z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path>
               </svg>
             </div>
             <div class="flex flex-col justify-center items-center">
-              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">20 +</dt>
-              <dd class="font-light text-gray-500 dark:text-gray-400">Nouveaux clients</dd>
+              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">3 958 €</dt>
+              <dd class="font-light text-gray-500 dark:text-gray-400">Générés</dd>
             </div>
           </div>
-          <div class="flex flec-row justify-center items-center align-middle bg-custom-white py-10 rounded">
+          <div class="flex flex-row justify-center items-center align-middle bg-custom-white py-10 rounded">
+            <div class="w-10 p-7 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-primary-900 -translate-x-1/2">
+              <svg class="w-9 -translate-y-1/2 -translate-x-1/2 text-[#3A36DB] dark:text-primary-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
+              </svg>
+            </div>
+            <div class="flex flex-col justify-center items-center">
+              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">60</dt>
+              <dd class="font-light text-gray-500 dark:text-gray-400">Clients actifs</dd>
+            </div>
+          </div>
+          <div class="flex flex-row justify-center items-center align-middle bg-custom-white py-10 rounded">
             <div class="w-10 p-7 h-10 rounded-full bg-pink-100 lg:h-12 lg:w-12 dark:bg-primary-900 -translate-x-1/2">
               <svg class="w-10 -translate-y-1/2 -translate-x-1/2 text-[#FF69B4] dark:text-primary-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 10.5V6a3.75 3.75 0 10-7.5 0v4.5m11.356-1.993l1.263 12c.07.665-.45 1.243-1.119 1.243H4.25a1.125 1.125 0 01-1.12-1.243l1.264-12A1.125 1.125 0 015.513 7.5h12.974c.576 0 1.059.435 1.119 1.007zM8.625 10.5a.375.375 0 11-.75 0 .375.375 0 01.75 0zm7.5 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"></path>
               </svg>
             </div>
             <div class="flex flex-col justify-center items-center">
-              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">190 +</dt>
-              <dd class="font-light text-gray-500 dark:text-gray-400">Nouveaux prospects</dd>
+              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">1878</dt>
+              <dd class="font-light text-gray-500 dark:text-gray-400">Prospects</dd>
             </div>
           </div>
-          <div class="flex flec-row justify-center items-center align-middle bg-custom-white py-10 rounded">
-            <div class="w-10 p-7 h-10 rounded-full bg-blue-100 lg:h-12 lg:w-12 dark:bg-primary-900 -translate-x-1/2">
-              <svg class="w-10 -translate-y-1/2 -translate-x-1/2 text-blue-600 dark:text-primary-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 00.75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 00-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0112 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 01-.673-.38m0 0A2.18 2.18 0 013 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 013.413-.387m7.5 0V5.25A2.25 2.25 0 0013.5 3h-3a2.25 2.25 0 00-2.25 2.25v.894m7.5 0a48.667 48.667 0 00-7.5 0M12 12.75h.008v.008H12v-.008z"></path>
+          <div class="flex flex-row justify-center items-center align-middle bg-custom-white py-10 rounded">
+            <div class="w-10 p-7 h-10 rounded-full bg-red-100 lg:h-12 lg:w-12 dark:bg-primary-900 -translate-x-1/2">
+              <svg class="w-10 -translate-y-1/2 -translate-x-1/2 text-red-600 dark:text-primary-300" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                <path stroke-linecap="round" stroke-linejoin="round" d="M15.362 5.214A8.252 8.252 0 0112 21 8.25 8.25 0 016.038 7.048 8.287 8.287 0 009 9.6a8.983 8.983 0 013.361-6.867 8.21 8.21 0 003 2.48z"></path>
+                <path stroke-linecap="round" stroke-linejoin="round" d="M12 18a3.75 3.75 0 00.495-7.467 5.99 5.99 0 00-1.925 3.546 5.974 5.974 0 01-2.133-1A3.75 3.75 0 0012 18z"></path>
               </svg>
             </div>
             <div class="flex flex-col justify-center items-center">
-              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">12 +</dt>
-              <dd class="font-light text-gray-500 dark:text-gray-400">Refferals</dd>
+              <dt class="mb-2 text-3xl md:text-4xl font-extrabold">70 %</dt>
+              <dd class="font-light text-gray-500 dark:text-gray-400">Objectif atteint</dd>
             </div>
           </div>
         </dl>
