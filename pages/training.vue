89910ababc58027add412475a6a303873c619370
jehanebnj <jehanebnj@gmail.com>
2023-06-10 20:54:21 +0200

    change button

--- pages/training.vue
+++ pages/training.vue
@@ -218,7 +218,7 @@ const chartOptionsRadar = ref({
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">{{ training.description }}</p>
               <a href="#"
                  class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-green rounded-lg hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Voir plus
+                S'inscrire
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
                   <path fill-rule="evenodd"
