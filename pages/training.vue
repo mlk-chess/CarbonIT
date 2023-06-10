aa133e6fc95e25f45d1322fbf9e8aaf644a401e2
jehanebnj <jehanebnj@gmail.com>
2023-06-10 20:21:07 +0200

    clean button color

--- pages/training.vue
+++ pages/training.vue
@@ -200,7 +200,7 @@ onMounted(() => {
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
                 acquisitions of 2021 so far, in reverse chronological order.</p>
               <a href="#"
-                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-button rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-green rounded-lg hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Voir plus
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
@@ -227,7 +227,7 @@ onMounted(() => {
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
                 acquisitions of 2021 so far, in reverse chronological order.</p>
               <a href="#"
-                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-button rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-green rounded-lg hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Voir plus
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
@@ -254,7 +254,7 @@ onMounted(() => {
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
                 acquisitions of 2021 so far, in reverse chronological order.</p>
               <a href="#"
-                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-button rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-green rounded-lg hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Voir plus
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
@@ -281,7 +281,7 @@ onMounted(() => {
               <p class="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology
                 acquisitions of 2021 so far, in reverse chronological order.</p>
               <a href="#"
-                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-button rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
+                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-custom-green rounded-lg hover:bg-green-900 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
                 Voir plus
                 <svg aria-hidden="true" class="w-4 h-4 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20"
                      xmlns="http://www.w3.org/2000/svg">
