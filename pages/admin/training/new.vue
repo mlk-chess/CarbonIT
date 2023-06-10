0c62fc90f7739aee7c488c03a3760d8fcc873d79
jehanebnj <jehanebnj@gmail.com>
2023-06-10 19:17:03 +0200

    create list of trainings

--- pages/admin/training/new.vue
+++ pages/admin/training/new.vue
@@ -50,7 +50,15 @@ async function saveTraining() {
                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
                    placeholder="description" required=""></textarea>
           </div>
-
+          <div>
+            <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Statut</label>
+            <select v-model="status" id="status"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+              <option selected value="0">À venir</option>
+              <option value="1">En cours</option>
+              <option value="2">Terminé</option>
+            </select>
+          </div>
 
           <div class="w-full">
             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
@@ -68,8 +76,8 @@ async function saveTraining() {
         
         </div>
         <button type="submit"
-                class="mt-7 text-white bg-custom-red hover:bg-red-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-          Ajouter
+                class="text-white bg-custom-green mt-5 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-custom-black hover:text-white">
+          Créer
         </button>
       </form>
     </div>
