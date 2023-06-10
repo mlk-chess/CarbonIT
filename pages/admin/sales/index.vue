f4405dad2f17b586ae97d3244262c2f619faa961
Théo Sigaud <sigaudtheo@gmail.com>
2023-06-10 13:00:13 +0200

    Add delete customer

--- pages/admin/sales/index.vue
+++ pages/admin/sales/index.vue
@@ -13,8 +13,14 @@ useHead({
 const customers = ref([]);
 const supabase = useSupabaseClient();
 const details = ref(false);
+const showModal = ref(false);
+const idCustomer = ref(null);
 
 onMounted(async () => {
+  getData();
+});
+
+async function getData() {
   const data = await $fetch('/api/sales/get', {
     method: 'get',
   });
@@ -22,7 +28,20 @@ onMounted(async () => {
   if (data !== 'Error') {
     customers.value = data;
   }
-});
+}
+
+async function deleteCustomer() {
+  const data = await $fetch('/api/sales/delete', {
+    method: 'delete',
+    body: JSON.stringify({
+      id: idCustomer.value
+    })
+  });
+
+  if (data !== 'Error') {
+    getData();
+  }
+}
 </script>
 
 <template>
@@ -226,7 +245,7 @@ onMounted(async () => {
                   <td class="px-6 py-4">
                     <span class="flex w-3 h-3 bg-green-500 rounded-full"></span>
                   </td>
-                  <td class="px-6 py-4">
+                  <td class="px-6 py-4 flex">
                     <NuxtLink :href="`/admin/sales/${customer.id}`" class="hover:cursor-pointer hover:text-blue-400">
                       <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
                            xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
@@ -236,6 +255,13 @@ onMounted(async () => {
                               d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"></path>
                       </svg>
                     </NuxtLink>
+                    <button @click="showModal = true; idCustomer = customer.id" class="ml-5 hover:cursor-pointer hover:text-red-400">
+                      <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="1.5" viewBox="0 0 24 24"
+                           xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                        <path stroke-linecap="round" stroke-linejoin="round"
+                              d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
+                      </svg>
+                    </button>
                   </td>
                 </tr>
                 </tbody>
@@ -245,7 +271,8 @@ onMounted(async () => {
         </div>
         <div class="col-span-12 xl:col-span-3 mt-10 mx-auto w-full">
           <h2 class="text-xl font-bold text-gray-900 dark:text-white">To do list :</h2>
-          <p class="font-light text-sm text-gray-500 dark:text-gray-400">Ces objectifs sont mis a jours quotidienement </p>
+          <p class="font-light text-sm text-gray-500 dark:text-gray-400">Ces objectifs sont mis a jours
+            quotidienement </p>
 
           <div
               class="w-80 xl:w-full flex flex-row justify-center items-center align-middle bg-custom-white py-2 mt-5 rounded">
@@ -312,6 +339,33 @@ onMounted(async () => {
               <dt class="mb-2 text-lg text-gray-700 md:text-xl">Contrat</dt>
               <dt class="mb-2 text-xl md:text-2xl text-gray-700 font-extrabold">2</dt>
               <dd class="font-light text-gray-500 dark:text-gray-400">restants aujourd'hui</dd>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div tabindex="-1" aria-hidden="true" v-show="showModal"
+           class="fixed top-0 left-0 right-0 z-50 w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
+        <div class="fixed top-0 left-0 right-0 z-40 h-full max-h-full bg-gray-400 opacity-50"></div>
+        <div class="relative w-full max-h-full z-50">
+          <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 mx-auto max-w-xl mt-32">
+            <button @click="showModal=false" type="button"
+                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+            >
+              <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"></path>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+            <div class="p-6 text-center">
+              <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+              <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Voulez-vous vraiment supprimer ce client ?</h3>
+              <button @click="deleteCustomer(); showModal = false" type="button" class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">
+                Oui, je suis sûr
+              </button>
+              <button @click="showModal = false" type="button" class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">Non, annuler</button>
             </div>
           </div>
         </div>
