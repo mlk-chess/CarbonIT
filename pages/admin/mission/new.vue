6c511b9f2eb5923723a13461d44f4f694fcff69d
jehanebnj <jehanebnj@gmail.com>
2023-06-11 11:51:18 +0200

    create mission form

--- pages/admin/mission/new.vue
+++ pages/admin/mission/new.vue
@@ -1,4 +1,5 @@
 <script setup>
+
 definePageMeta({
   middleware: ["auth-admin"],
   layout: "user"
@@ -10,68 +11,85 @@ useHead({
   }
 });
 
+const title = ref("");
+const description = ref("");
+const date_startMission = ref(null);
+const time_startMission = ref(null);
+
+
+async function saveTraining() {
+    
+  await $fetch('/api/mission/new', {
+    method: 'post',
+    body: {
+      title: title.value,
+      description: description.value,
+      date_startMission : date_startMission.value,
+      time_startMission : time_startMission.value
+    }
+  });
+}
 </script>
 
 <template>
   <section>
     <div class="py-8 px-4 mx-auto max-w-2xl lg:py-16">
-      <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Nouvelle mission</h2>
-      <form v-on:submit.prevent="saveUser">
-        <div class="grid gap-4 sm:grid-cols-2 sm:gap-6"> 
-            <div class="w-full">
+      <h2 class="mb-4 text-xl font-bold text-gray-900 dark:text-white">Créer une mission</h2>
+
+      <form v-on:submit.prevent="saveTraining">
+        <div class="grid gap-4 sm:grid-cols-1 sm:gap-6">
+          <div class="w-full">
             <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Titre de la mission</label>
             <input v-model="title" type="text" name="title" id="title"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-red focus:border-custom-red block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Titre de l'objectif" required="">
-            </div>
-            <div class="w-full">
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Titre" required="">
+          </div>
+          <div class="w-full">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Entreprise d'accueil</label>
+            <input v-model="title" type="text" name="title" id="title"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Entreprise" required="">
+          </div>
+          <div class="w-full">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Où est situé la mission ?</label>
+            <input v-model="title" type="text" name="title" id="title"
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Localisation" required="">
+          </div>
+          <div class="w-full">
             <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Description</label>
             <textarea v-model="description" type="text" name="description" id="description"
-                    class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-red focus:border-custom-red block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    placeholder="Description de l'objectif" required=""></textarea>
-            </div>
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                   placeholder="Description" required=""></textarea>
+          </div>
           <div>
             <label for="status" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Statut</label>
             <select v-model="status" id="status"
                     class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-              <option selected value="0">Collaborateur</option>
-              <option value="1">Administrateur</option>
+              <option selected value="0">À venir</option>
+              <option value="1">En cours</option>
+              <option value="2">Terminé</option>
             </select>
           </div>
-          <div>
-            <label for="phone" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Téléphone</label>
-            <input v-model="phone" type="tel" name="phone" id="phone"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                   placeholder="12" required="">
-          </div>
-          <div class="sm:col-span-2">
-            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-            <input v-model="email" type="email" name="name" id="name"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                   placeholder="Email du collaborateur" required="">
-          </div>
-          <div class="sm:col-span-2">
-            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Adresse</label>
-            <input v-model="address" type="text" name="name" id="name"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                   placeholder="Adresse du collaborateur" required="">
-          </div>
+
           <div class="w-full">
-            <label for="firstname" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Ville</label>
-            <input v-model="city" type="text" name="firstname" id="firstname"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                   placeholder="Ville de l'utilisateur" required="">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date</label>
+            <input v-model="dateTraining" type="date" 
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    required="">
           </div>
-          <div class="w-full">
-            <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Code Postal</label>
-            <input v-model="zip" type="text" name="name" id="name"
-                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-custom-blue focus:border-custom-blue block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                   placeholder="Code postal de l'utilisateur" required="">
+
+           <div class="w-full">
+            <label for="title" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Heure</label>
+            <input v-model="timeTraining" type="time" name="" id=""
+                   class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                    required="">
           </div>
+        
         </div>
         <button type="submit"
-                class="mt-7 text-white bg-custom-red hover:bg-red-900 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">
-          Créer
+                class="text-white bg-custom-green mt-5 font-medium rounded-lg text-sm px-5 py-2.5 text-center hover:bg-custom-black hover:text-white">
+          Créer une mission
         </button>
       </form>
     </div>
